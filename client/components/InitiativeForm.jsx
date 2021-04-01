8b05f3193097245f92b73263f15a23d569d20cc8
chris-paul-ejercito <chris.paul.ejercito@gmail.com>
2021-04-01 16:00:03 -0700

    added delete button

--- client/components/InitiativeForm.jsx
+++ client/components/InitiativeForm.jsx
@@ -1,89 +1,35 @@
 import React, { Component, useState } from 'react';
 
 const InitiativeForm = function(props) {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     name: '',
-  //     initiative: 0,
-  //   };
+  const { setName, setInitiative, handleSubmit, getCreatures } = props;
 
-  //   this.handleChangeName = this.handleChangeName.bind(this);
-  //   this.handleChangeInitiative = this.handleChangeInitiative.bind(this);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  //   this.handleTest = this.handleTest.bind(this);
-  // }
-
-  // handleChangeName(event) {
-  //   this.setState({ name: event.target.value });
-  // }
-
-  // handleChangeInitiative(event) {
-  //   this.setState({ initiative: event.target.value });
-  // }
-
-  const handleSubmit = (event) => {
-    const { name, initiative } = props;
-    event.preventDefault();
-    fetch('/api/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'Application/JSON',
-      },
-      body: JSON.stringify({ name, initiative }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch(err => console.log('could not send to server', err));
-  };
-
-  const handleTest = (event) => {
-    event.preventDefault();
-    fetch('/api/', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'Application/JSON',
-      },
-    })
-      .then(res => res.json())
-      .then((data) => {
-        console.log('handleTest', data);
-        // console.log('creatures', props.creatures);
-        console.log('props.name', props.name);
-      })
-      .catch(err => console.log('could not GET from server', err));
-  };
-
-  // render() {
-    return (
-      <form onSubmit={handleSubmit}>
-        <h3>Initiative Form</h3>
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Initiative Form</h3>
+      <label>
+        Name:
+        <input
+          name="name"
+          type="text"
+          onChange={(event) => setName(event.target.value)}
+          required
+        />
+      </label>
+      <div>
         <label>
-          Name:
+          Initiative:
           <input
-            name="name"
-            type="text"
-            onChange={(event) => props.setName(event.target.value)}
+            name="initiative"
+            type="number"
+            onChange={(event) => setInitiative(event.target.value)}
+            required
           />
         </label>
-        <div>
-          <label>
-            Initiative:
-            <input
-              name="initiative"
-              type="number"
-              value={props.initiative}
-              onChange={props.setInitiative}
-            />
-          </label>
-        </div>
-        <div><input type="submit" value="Submit" /></div>
-        <div><button type="button" onClick={handleTest}>Test GET</button></div>
-      </form>
-    );
-  // }
-}
+      </div>
+      <div><input type="submit" value="Submit" /></div>
+      <div><button type="button" onClick={(event) => getCreatures(event)}>Test GET</button></div>
+    </form>
+  );
+};
 
 export default InitiativeForm;
--- client/components/InitiativesDisplay.jsx
+++ client/components/InitiativesDisplay.jsx
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 import Creature from '../containers/CreatureDisplay.jsx';
+import Initiative from './InitiativeDisplay.jsx';
 
-const InitiativesDisplay = function(props) {
+const InitiativesDisplay = (props) => {
   // constructor(props) {
   //   super(props);
   // }
+  const { creatures } = props;
+  const initiatives = new Set();
 
-  // render() {
-    return (
-      <div>
-        <h3>Initiatives Display</h3>
-        <Creature
-          name="creature 1"
-          dmgtaken={4}
-          ac={10}
-        />
-      </div>
+  const test = creatures.reduce((accumulator, creature) => {
+    initiatives.add(creature.initiative);
+    const {
+      _id, name, dmgtaken, ac, initiative,
+    } = creature;
+    const component = (
+      <Creature
+        key={_id}
+        _id={_id}
+        name={name}
+        dmgtaken={dmgtaken}
+        ac={ac}
+      />
     );
+    if (!Object.prototype.hasOwnProperty.call(accumulator, initiative)) {
+      accumulator[initiative] = [component];
+    } else {
+      accumulator[initiative].push(component);
+    }
+    return accumulator;
+  }, {});
+
+  const initiativeComponents = Array.from(initiatives).map((initiative) => (
+    <Initiative
+      key={`init_${initiative}`}
+      initiative={initiative}
+    >
+      {test[initiative]}
+    </Initiative>
+  ));
+
+  return (
+    <div>
+      <h3>Initiatives Display</h3>
+      {initiativeComponents}
+    </div>
+  );
   // }
 }
 
